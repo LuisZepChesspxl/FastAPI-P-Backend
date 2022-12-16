ae5b21e3f81d7f21571676655a10bf87c4fb97c1
Luis Zepeda <wimar.doc@gmail.com>
2022-12-15 18:39:44 -0600

    Formulario de la aplicacion

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from typing import Union 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import json
 
-from models.item_model import Item
+app = FastAPI()
 
-# creacion de una aplicacion
+app.mount("/static", StaticFiles(directory="./public/static"), name="static")
 
-app= FastAPI()
+templates = Jinja2Templates(directory="./public/templates")
 
-@app.get("/")
-def read_root():
-    return {"si yo digo que si": "mundo"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.get('/calculador')
-def calcular(operador_1: float, operador_2: float ):
-    return {'suma': operador_1 + operador_2}
-
-@app.put('/items/({item_id}')
-def update_item(item_id: int, item: Item):
-    return {'item_name': item.name, 'item_id': item_id, 'item_price':item.price}
+@app.get('/index/', response_class=HTMLResponse)
+def index(request: Request):
+    context = {'request': request}
+    return templates.TemplateResponse("index.html", context)
